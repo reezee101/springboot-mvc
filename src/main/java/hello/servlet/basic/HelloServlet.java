8ea6bf78918f79a07183c8cfd4e349a313b4fff9
reezee101 <reze101@naver.com>
2023-02-13 23:32:04 +0900

    2일차

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -7,11 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")  //중복되면 안됨
 public class HelloServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
+        System.out.println(request);
+        System.out.println(response);
+        String userName = request.getParameter("userName");
+        System.out.println(userName);
+
+        response.setContentType("text/plane");  //헤더
+        response.setCharacterEncoding("utf-8"); //헤더
+        response.getWriter().write("Hello" + userName); //http body에 들어갈 값
     }
 }
-
